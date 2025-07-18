6f2a27efa8922bd1d4b18fe7a9d0c20daf26ee72
David Shaw <david.shaw@groups360.com>
2025-07-18 12:41:06 -0500

    added CORS

--- BE/app.py
+++ BE/app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from BE.routes import router
 
 app = FastAPI()
 
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
