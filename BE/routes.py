ffa4eff7905eed7dbe41015dbc76276c15831915
David Shaw <david.shaw@groups360.com>
2025-07-18 09:39:11 -0500

    added comments

--- BE/routes.py
+++ BE/routes.py
@@ -11,3 +11,33 @@ def health_check():
 @router.get("/test")
 def test_endpoint():
     return {"status": "ok", "message": "Test endpoint is working."}
+
+
+# Endpoint #1
+@router.get("/authors")
+def get_authors():
+    return [{"name": "Dummy Author 1", "email": "dummy@author.com"}]
+
+
+# Endpoint #2
+@router.get("/outliers")
+def get_outliers():
+    return [
+        {
+            "sha": 123456,
+            "title": "Large commit message",
+            "author": {"name": "Dummy Author 1", "email": "dummy@author.com"},
+        }
+    ]
+
+
+# Endpoint #3
+@router.get("/snapshot/{type}")
+def get_snapshot(type: str):
+    return {}
+
+
+# Endpoint #4
+@router.get("/commit-search-count")
+def get_commit_search_count():
+    return {}
